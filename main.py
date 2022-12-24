10a8d83e20c98ec60080ca8e8e50eb0bc566825a
Yank-My-Chain <53712387+Yank-My-Chain@users.noreply.github.com>
2022-12-23 20:21:34 -0500

    continuing classes

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
+# Symbols for squares
+UNTOUCHED = '-'
+BLANK = 'O'
+FLAGGED = '/'
+BOMB = 'X'
+
+# Field size
+WIDTH = 10
+HEIGHT = 10
+
 class Cell:
     def __init__(self):
         self.contains_bomb = False
         self.touching_bombs = 0
         self.flagged = False
         self.revealed = False
+        self.front_view = ''
+
+    def reveal(self):
+        pass
+
+    def flag(self):
+        pass
+
 
 class MineField:
-    pass
+    def __init__(self):
+        pass
+    
+    def print_field(self):
+        pass
+
+
